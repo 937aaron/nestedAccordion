38e8bf4f3231a9471d3a3a0a7aff3596f2cde81c
Aaron <newcomer.aaron@icloud.com>
2016-05-28 09:02:00 -0400

    Added second entry and dropdown feature

--- assets/nestedAccordion.js
+++ assets/nestedAccordion.js
@@ -1,11 +1,45 @@
-var paragraph = document.querySelector(".paragraph");
-var openHeader = document.querySelector("headList");
+// var paragraph = document.querySelector(".paragraph");
+// var openHeader = document.querySelector("headList");
+//
+//   openHeader.eventListener("click", reveal);
+//
+// function reveal(){
+//   paragraph.style.diplay = "block"
+// }
+//
+//
+// document.querySelector(".header").eventListener("hover",rotateColor)
 
-  openHeader.eventListener("click", reveal);
 
-function reveal(){
-  paragraph.style.diplay = "block"
+// document.getElementByClass("dropdown").addEventListener("click", dropFunction);
+// document.getElementById("button2").addEventListener("click", dropFunction);
+// var paragraphShow = document.querySelector(".paragraph");
+// var showSmall = document.querySelector(".hidesmall");
+// function dropFunction (){
+//   paragraphShow.className = "paragraphShow";
+//   showSmall.className="";
+//   showSmall.className="smallList";
+//
+// };
+ x = 1+1
+ console.log(x);
+
+var drops = document.getElementsByClassName("header");
+//var drops = document.querySelector(".header");
+var paragraphShow = document.querySelector(".paragraph");
+var showSmall = document.querySelector(".hidesmall");
+//drops.addEventListener("click", dropFunction);
+// drops.addEventListener("click", myFunction);
+// function myFunction (){
+  // alert("heygirl");
+// };
+
+  for (var i = 0; i < drops.length; i++){
+    drops[i].addEventListener("click", function(){
+    this.nextElementSibling.classList.toggle("show");
+    //document.getElementById("paragraph").nextElementSibling.classList.toggle("smallList");
+    //showSmall.className="smallList";
+  });
 }
 
-
-document.querySelector(".header").eventListener("hover",rotateColor)
+//drops.addEventListener("click", dropFunction);
